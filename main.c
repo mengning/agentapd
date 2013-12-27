74c3f94b8748aee2e9bb45dc038846919a06aaf9
mengning <mengning@ustc.edu.cn>
2013-12-27 12:34:57 +0800

    添加了少许模仿hostapd调用nl80211 driver的代码范例

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 #include<string.h>			/* memset */
 
 #include"driver.h"
+extern struct wpa_driver_ops *wpa_drivers[];
 /*
  * 模仿hostapd调用driver
  * 注意call-down和driver call-up
+ * call-down至少包括nl80211_global_init/nl80211_global_deinit,i802_init/i802_deinit
+ * call-up至少包括wpa_supplicant_event,wpa_scan_results_free
  */
 int main() 
 {
+    int i = 0;
+	for (i = 0; wpa_drivers[i]; i++) 
+	{
+		if (wpa_drivers[i]->global_init) 
+		{
+			void * p = wpa_drivers[i]->global_init();
+			if (p == NULL) {
+				printf("Failed to initialize\n");
+				return -1;
+			}
+		}
+	}
+	printf("NL80211 initialized\n");
     return 0;
 }
